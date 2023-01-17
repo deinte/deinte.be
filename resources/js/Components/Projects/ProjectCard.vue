6a429e4d7de4f2c8f07ac1b2ff7db976b822e28c
Dante Schrauwen <contact@danteschrauwen.be>
2023-01-17 21:46:34 +0100

    updated cards

--- resources/js/Components/Projects/ProjectCard.vue
+++ resources/js/Components/Projects/ProjectCard.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="text-center">
-    <img :srcset="project.cover" alt="" class="mx-auto">
+    <div class="text-center">
+        <div class="aspect-video">
+            <img :srcset="project.cover" alt="" class="w-full h-full object-cover">
+        </div>
 
-    <h3 class="text-2xl font-bold mt-2">{{ project.title }}</h3>
-    <p class="mt-1">{{ project.subtitle }}</p>
-  </div>
+        <h3 class="text-2xl font-bold mt-2">{{ project.title }}</h3>
+        <p class="mt-1">{{ project.subtitle }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "ProjectCard",
+    name: "ProjectCard",
 
-  props: {
-    project: {
-      type: Object,
-      required: true,
+    props: {
+        project: {
+            type: Object,
+            required: true,
+        }
     }
-  }
 }
 </script>
 
